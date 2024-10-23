ec29fe259f3f32d3142d752981982bcd6c836fe7
shikha bhadoria <145272515+shikhabhadoria@users.noreply.github.com>
2024-10-24 03:56:29 +0530

    Add files via upload

---
+++ Day21 - IPL and RCB.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--){
+	    int x;
+	    cin>>x;
+	    int y;
+	    cin>>y;
+	    
+	    int minwins = max(0 , x-y);
+	    cout<<minwins<<endl;
+	    
+	}
+
+}
