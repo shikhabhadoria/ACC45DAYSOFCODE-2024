ed20b4fc18af871747a8d95d2c6501876313b94a
shikha bhadoria <145272515+shikhabhadoria@users.noreply.github.com>
2024-11-09 14:51:45 +0530

    Add files via upload

---
+++ Day36 - Stick break.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--){
+	    long long L;
+	    cin>>L;
+	    long long K;
+	    cin>>K;
+	    if(L % K == 0){
+	        cout<<0<<endl;
+	    }
+	    else{
+	        cout<<1<<endl;
+	    }
+	}
+
+}
